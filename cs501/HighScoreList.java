9e3eb483303689b258a778657c9b03ef1b0ac2f5
John Loesel <skelter2014gm@gmail.com>
2022-02-19 15:00:01 -0500

    refactored linked list to use only generics

---
+++ cs501/HighScoreList.java
@@ -0,0 +1,130 @@
+package cs501;
+
+public class HighScoreList<E> {
+
+    SinglyLinkedList<GameEntry> list;
+
+
+    public static void main(String[] args) {
+
+        HighScoreList<Integer> highScore = new HighScoreList<>();
+
+        int[] a = new int[] { 1,10,2,9,8,3,7,4,5,6,3  };
+        highScore.list = new SinglyLinkedList<>();
+
+        for (int i : a) {
+
+            GameEntry entry = new GameEntry("Score_"+i, i);
+            if (i % 2 == 0){
+                highScore.addNewScore( entry);
+            } else {
+                highScore.addNewScore(entry);
+            }
+        }
+        highScore.printList();
+
+
+
+
+    }
+    /** Iterate the entire list and print the scores */
+        public void printList() {
+        if (list.isEmpty()) {
+            System.out.println("\n------------------[Empty list]--------------------------\n");
+        } else {
+            System.out.println("--------------------------------------------------------");
+            System.out.print("head: " + list.first().getElement() + "\ttail: " + list.last().getElement() + "\n");
+            System.out.println("--------------------------------------------------------");
+            Node<GameEntry> pointer = list.first();
+            while (pointer != null) {
+                System.out.println(pointer.getElement() + " ");
+                pointer = pointer.getNext();
+            }
+        }
+
+    }
+
+    
+        /** Adds a new score node to the list in the proper DEC SORTED position */
+    public void addNewScore(GameEntry entry) {
+
+        Node<GameEntry> pointer = list.first();
+        // pointer = list.head;
+        int ptrScore = pointer != null ? ((GameEntry)pointer.getElement()).score : 0;
+
+        // is list empty or is it bigger than head? just add to head
+        if (list.isEmpty() || entry.score > ptrScore) {
+            list.addFirst( entry) ;
+        } else {
+            // find the spot to insert
+            while (pointer != list.last().getNext() && entry.score < ((GameEntry)pointer.getNext().getElement()).score) {
+                pointer = pointer.getNext();
+            }
+
+            if (pointer == list.last().getNext()) {
+                list.addLast(entry);
+            } else {
+                // found the spot to insert between head and tail
+                list.insert(entry, pointer);
+            }
+        }
+        while (list.size() > 10) {
+            list.removeLastNode();
+        }
+    }
+    
+    // public static void printList(SinglyLinkedList<GameEntry> list) {
+    //     if (list.isEmpty()) {
+    //         System.out.println("\n------------------[Empty list]--------------------------\n");
+    //     } else {
+    //         System.out.println("--------------------------------------------------------");
+    //         System.out.print("head: " + list.head.element + "\ttail: " + list.tail.element + "\n");
+    //         System.out.println("--------------------------------------------------------");
+    //         Node<GameEntry> pointer = list.first();
+    //         pointer = list.head;
+    //         while (pointer != null) {
+    //             System.out.println(pointer.getElement() + " ");
+    //             pointer = pointer.getNext();
+    //         }
+    //     }
+
+    // }
+
+    // /** Class to score Player's Name and High Score */
+    private static class GameEntry {
+        private String name;
+        private int score;
+
+        public GameEntry(String n, int s) {
+            name = n;
+            score = s;
+        }
+
+        // #region Getters & Setters
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        public void setScore(int score) {
+            this.score = score;
+        }
+
+        // #endregion
+
+        /** Returns a formatted string representation of this entry. */
+        public String toString() {
+            return String.format("%s :    %02d", name, score);
+        }
+    }
+
+
+    
+}
--- cs501/SinglyLinkedList.java
+++ cs501/SinglyLinkedList.java
@@ -3,7 +3,7 @@ package cs501;
 public class SinglyLinkedList<E> {
 
     // instance variables
-    SinglyLinkedList<GameEntry> _scores;
+    // SinglyLinkedList<GameEntry> _scores;
     private Node<E> head = null;
     private Node<E> tail = null;
     private int size = 0;
@@ -13,64 +13,8 @@ public class SinglyLinkedList<E> {
         head = null;
         tail = null;
         size = 0;
-        _scores = (SinglyLinkedList<GameEntry>) this; // reference to self
     }
 
-    public static void main(String[] args) {
-        int[] a = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        SinglyLinkedList<GameEntry> list = new SinglyLinkedList<>();
-
-        // Load with 10 scores of test data
-        printList(list);
-        for (int i = 0; i < a.length; i++) {
-            list.addNewScore(new GameEntry("Score_" + (i + 1), a[i]));
-        }
-        printList(list);
-
-        // Add insert a new score into the list
-        list.addNewScore(new GameEntry("Score_X", 7));
-        printList(list);
-
-        // Test removing 8 nodes
-        while (list.size > 2) {
-            list.removeNode(1);
-        }
-        printList(list);
-        // add a couple more
-        list.addNewScore(new GameEntry("Test", 11));
-        list.addFirst(new GameEntry("First", 2)); // Add directly to head. Not subject to sort
-        list.addLast(new GameEntry("Last", 99));// Add directly to tail. Not subject to sort.
-
-        printList(list);
-
-        // test the removeNode method
-        if (!list.isEmpty()) {
-            list.removeNode(1);
-            list.removeNode(1);
-            list.removeNode(1);
-        }
-
-        printList(list);
-
-    }
-
-    /** Iterate the entire list and print the scores */
-    public static void printList(SinglyLinkedList<GameEntry> list) {
-        if (list.isEmpty()) {
-            System.out.println("\n------------------[Empty list]--------------------------\n");
-        } else {
-            System.out.println("--------------------------------------------------------");
-            System.out.print("head: " + list.head.element + "\ttail: " + list.tail.element + "\n");
-            System.out.println("--------------------------------------------------------");
-            Node<GameEntry> pointer = list.head;
-            pointer = list.head;
-            while (pointer != null) {
-                System.out.println(pointer.element + " ");
-                pointer = pointer.next;
-            }
-        }
-
-    }
 
     /** Removes the nth node in the list */
     public void removeNode(int i) {
@@ -83,50 +27,23 @@ public class SinglyLinkedList<E> {
         } else if (i == size) {
             removeLastNode();
         } else {
-            Node<E> pointer = head.next;
+            Node<E> pointer = head.getNext();
             int j = 2;
 
             while (i > j + 1) {
-                pointer = pointer.next;
+                pointer = pointer.getNext();
                 j++;
             }
-            if (pointer.next == tail) {
+            if (pointer.getNext() == tail) {
                 removeLastNode();
             } else {
-                pointer.next = pointer.next.next;
+                pointer.setNext(null);
             }
             size--;
         }
 
     }
 
-    /** Adds a new score node to the list in the proper DEC SORTED position */
-    public void addNewScore(GameEntry entry) {
-
-        Node<GameEntry> pointer = _scores.head;
-        // pointer = list.head;
-        int ptrScore = pointer != null ? pointer.element.score : 0;
-
-        // is list empty or is it bigger than head? just add to head
-        if (isEmpty() || entry.score > ptrScore) {
-            _scores.addFirst(entry);
-        } else {
-            // find the spot to insert
-            while (pointer != tail && entry.score < pointer.next.element.score) {
-                pointer = pointer.next;
-            }
-
-            if (pointer == _scores.tail) {
-                _scores.addLast(entry);
-            } else {
-                // found the spot to insert between head and tail
-                _scores.insert(entry, pointer);
-            }
-        }
-        while (size > 10) {
-            removeLastNode();
-        }
-    }
 
     /** Removes the last node of the list */
     public void removeLastNode() {
@@ -135,10 +52,10 @@ public class SinglyLinkedList<E> {
         if (head == null) {
             return;
         }
-        while (ptr.next != tail) {
-            ptr = ptr.next;
+        while (ptr.getNext() != tail) {
+            ptr = ptr.getNext();
         }
-        ptr.next = null;
+        ptr.setNext(null);
         tail = ptr;
         size--;
     }
@@ -152,18 +69,18 @@ public class SinglyLinkedList<E> {
         return size == 0;
     }
 
-    public E first() {
+    public Node<E> first() {
         if (isEmpty()) {
             return null;
         }
-        return head.getElement();
+        return head;
     }
 
-    public E last() {
+    public Node<E> last() {
         if (isEmpty()) {
             return null;
         }
-        return tail.getElement();
+        return tail;
     }
 
     /** Add a score directly to head of list. Does not ensure proper ordering. */
@@ -188,7 +105,7 @@ public class SinglyLinkedList<E> {
         if (isEmpty()) {
             head = newest;
         } else {
-            tail.next = newest; // connect new node to current tail.
+            tail.setNext(newest); // connect new node to current tail.
         }
         tail = newest; // new node becomes the tail
         size++;
@@ -215,66 +132,10 @@ public class SinglyLinkedList<E> {
             addFirst(e);
 
         } else {
-            prev.next = new Node<>(e, prev.next);
+            prev.setNext( new Node<>(e, prev.getNext()) );
             size++;
         }
     }
 
-    // nested Node Class
-    private static class Node<E> {
-        private E element;
-        private Node<E> next;
 
-        public Node(E e, Node<E> n) {
-            element = e;
-            next = n;
-        }
-
-        public E getElement() {
-            return element;
-        }
-
-        public Node<E> getNext() {
-            return next;
-        }
-
-        public void setNext(Node<E> n) {
-            next = n;
-        }
-    }
-
-    /** Class to score Player's Name and High Score */
-    private static class GameEntry {
-        private String name;
-        private int score;
-
-        public GameEntry(String n, int s) {
-            name = n;
-            score = s;
-        }
-
-        // #region Getters & Setters
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getScore() {
-            return score;
-        }
-
-        public void setScore(int score) {
-            this.score = score;
-        }
-
-        // #endregion
-
-        /** Returns a formatted string representation of this entry. */
-        public String toString() {
-            return String.format("%s :    %02d", name, score);
-        }
-    }
 }
