90effd8e9b5cf9be0856604852195fb1520a144d
John Loesel <skelter2014gm@gmail.com>
2022-04-27 09:00:23 -0400

    .

--- cs501/assignments/Assignment_5.java
+++ cs501/assignments/Assignment_5.java
@@ -11,15 +11,13 @@ For an additional challenge , allow the leaves to store variables of X1, X2 , X3
 which are initially 0 and which can be updated interactively by your program, with the corresponding 
 update in the printed value of the root of the expression tree.
 
-https://www.javatpoint.com/convert-infix-to-postfix-notation#:~:text=Rules%20for%20the%20conversion%20from%20infix%20to%20postfix%20expression&text=If%20the%20incoming%20symbol%20is,push%20it%20on%20the%20stack.
 Assumptions: 
-    1. expressions use variables (a,b,x,y) as operands 
+    1. expressions use variables (a,b,x,y) or single digits as operands 
     2. chars [* / + - ] as operators which have L - R associativity
     3, Parentheses are used to determine order of operations but are not part of the postfix final expression
-    4. All expressions are valid with invalid chars. - error checking can be added later. Spaces are ignored
-    5. Operands are single chars
-the order of the postfix expression 
+    4. All expressions are valid - error checking can be added later. Spaces are ignored
 
+    convert infix to postfix, convert to tree, print the tree and the root.
     Example:
         Infix   = (A+B)*(C-D)/E
         Postfix = AB+CD-*E/
@@ -28,13 +26,6 @@ the order of the postfix expression
                 +        /
               A   B     -   E
                       C  D
-   Convert Postfix to Tree
-        1. convert to postfix L,R, Root - 
-        2. create a leaf node for each operand you find and push onto stack
-        3. When you read an operator, create a node - this a a new root of a sub tree
-            3.a Pop the top of the stack and add as right child of operator node
-            3b. Pop the next node on the stack (now the top) and add as a right child of the operator sub-tree
-        4. push the new subtree back onto the stack. - this becomes the new left (node)
 */
 
 public class Assignment_5 {
@@ -64,6 +55,11 @@ public class Assignment_5 {
         System.out.println("-------------------------------------------------------------------------");
         System.out.print("infix: ");
         System.out.println(infixArrayA );
+        System.out.print("postOrder: [ ");
+        for (Position<Character> c : treeA.postorder()) {
+            System.out.print(c.getElement() + " ");
+        }
+        System.out.println("]");
         System.out.println("root: " + treeA.root.getElement());
         System.out.println("-----------------------------");
         printVerticalTree(treeA.root, 0);
@@ -71,12 +67,23 @@ public class Assignment_5 {
         System.out.println("-------------------------------------------------------------------------");
         System.out.print("infix: ");
         System.out.println(infixArrayB);
+        System.out.print("postOrder: [ ");
+        for (Position<Character> c : treeB.postorder()) {
+            System.out.print(c.getElement() + " ");
+        }
+        System.out.println("]");
         System.out.println("root: " + treeB.root.getElement());
         System.out.println("-----------------------------");
         printVerticalTree(treeB.root, 0);
         System.out.println("-------------------------------------------------------------------------");
         System.out.print("infix: ");
         System.out.println(infixArrayC);
+        System.out.print("postOrder: [ ");
+        for (Position<Character> c : treeC.postorder()) {
+            System.out.print(c.getElement() + " ");
+        }
+        System.out.println("]");
+
         System.out.println("root: " + treeC.root.getElement());
         System.out.println("-----------------------------");
         printVerticalTree(treeC.root,0);
@@ -145,12 +152,12 @@ public class Assignment_5 {
     }
 
     /**
-     * The LAST operator processed with be the root in postfix notation (L,R,Root)
+     * The last operator processed will; be the root in postfix notation (L,R,Root)
      */
     public static LinkedBinaryTree<Character> convertToTree(char[] postFix) {
 
-        LinkedBinaryTree.Node<Character> right;
         LinkedBinaryTree.Node<Character> left;
+        LinkedBinaryTree.Node<Character> right;
         LinkedBinaryTree.Node<Character> root = null;
 
         // The expression tree
@@ -164,10 +171,13 @@ public class Assignment_5 {
 
             // push letters or numbers directly to the stack
             if (Character.isLetterOrDigit(c)) {
-                LinkedBinaryTree.Node<Character> node = new LinkedBinaryTree.Node<>(c, null, null, null);
+                LinkedBinaryTree.Node<Character> node 
+                    = new LinkedBinaryTree.Node<>(c, null, null, null);
+
                 stack.push(node);
             }
             // when it is a operator, create a subtree and put back on stack
+            // stack.top() is right child. stack.top() - 1 is left child
             else {
                 right = (LinkedBinaryTree.Node<Character>) stack.pop();
                 left = (LinkedBinaryTree.Node<Character>) stack.pop();
@@ -178,6 +188,7 @@ public class Assignment_5 {
         }
 
         tree.setRoot(root);
+        //need to set the size of the tree as it was created outside of the tree class.
         tree.setSize(tree.numChildren(root));
         return tree;
 
@@ -200,30 +211,32 @@ public class Assignment_5 {
         return operators.indexOf(c1) - operators.indexOf(c2);
     }
 
-    // incoming operator index is higher (to the right) of top of stack
+    /* returns true if incoming operator index is higher (to the right) of top of stack */
     public static boolean isHigher(char c1, char c2) {
         return getPrededence(c1, c2) > 1;
     }
 
-    // incoming operator index is lower (to the left) of top of stack
+    /* returns true if incoming operator index is lower (to the left) of top of stack */
     public static boolean isLower(char c1, char c2) {
         return getPrededence(c1, c2) < 0;
     }
 
-    // diff between operator indexes is 0 or 1; ex. +- are only 1 index apart
+    /* returns true if diff between operator indexes is 0 or 1; ex. +- are only 1 index apart*/
     public static boolean isEqual(char c1, char c2) {
         int precedence = getPrededence(c1, c2);
         return precedence >= 0 && precedence <= 1;
     }
 
-    // Function to print binary tree
-    // It does reverse inorder traversal
+    /**prints the tree 90 degrees rotated to the left. The right most leaf is printed on top */
     static void printVerticalTree(LinkedBinaryTree.Node<Character> root, int space) {
         // Base case
         if (root == null)
             return;
 
-        // Increase distance between levels
+        // Increase distance between levels in increments of 10
+        //example
+        //           <space>      node
+        //    <space>      node
         space += COUNT;
 
         // Process right child first
